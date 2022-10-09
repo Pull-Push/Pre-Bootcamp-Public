f401233b1d8efbe807ba50fc92e072d5b6d730fa
Jeff Sokol <112549307+Pull-Push@users.noreply.github.com>
2022-10-08 20:28:05 -0400

    Upload Button-Clicker core assignment

---
+++ Button-Clicker/script.js
@@ -0,0 +1,12 @@
+function logMeIn(element){
+    element.innerText="Logout";
+}
+
+function alertLike(){
+    alert("Ninja was liked");
+}
+
+function hideMe(element){
+    element.remove();
+    
+}
